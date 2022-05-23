ee88dbc1c0c53209ef869de1280c0f4d25215508
dirkdirk <dirkdirk@users.noreply.github.com>
2022-05-23 11:05:38 -0700

    Update gitignore

---
+++ src/firebaseInit.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAWn1hBQySzWcJf4I1ldKPWHZs4AfwrNuw",
+  authDomain: "todo4lifedev.firebaseapp.com",
+  databaseURL: "https://todo4lifedev-default-rtdb.firebaseio.com",
+  projectId: "todo4lifedev",
+  storageBucket: "todo4lifedev.appspot.com",
+  messagingSenderId: "115067208580",
+  appId: "1:115067208580:web:7299be1a9a87418e3393d1",
+};
+
+initializeApp(firebaseConfig);
